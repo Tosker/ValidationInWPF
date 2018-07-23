e59a00db9fe5d292c06e063dc353d982a083325b
Joseph <josephebenge@gmail.com>
2018-07-23 19:12:58 -0400

    Initial commit with project templates

---
+++ DataValidation/B_Validation_ByDataErrorInfo/ObservableObject.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_Validation_ByDataErrorInfo
+{
+    /// <summary>
+    /// A base for objects using property notification.
+    /// </summary>
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Notify a property change
+        /// </summary>
+        /// <param name="propertyName">Name of property to update</param>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Notify a property change that uses CallerMemberName attribute
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="backingField">Backing field of property</param>
+        /// <param name="value">Value to give backing field</param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        protected virtual bool OnPropertyChanged<T>(ref T backingField, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(backingField, value))
+                return false;
+
+            backingField = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
---
+++ DataValidation/C_Validation_ByCustom/RegistrationVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Validation_ByCustom
+{
+    public class RegistrationVM : ObservableObject
+    {
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                OnPropertyChanged(ref _username, value);
+            }
+        }
+    }
+}
