eba7020cd9aa8feee73ed33bd7dca7e46aa51644
Joseph <josephebenge@gmail.com>
2018-07-23 19:21:05 -0400

    Added empty project w/ template for convenient viewer follow-along

---
+++ DataValidation/EmptyPracticeProject/RegistrationVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmptyPracticeProject
+{
+    public class RegistrationVM : ObservableObject
+    {
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set
+            {
+                OnPropertyChanged(ref _username, value);
+            }
+        }
+    }
+}
