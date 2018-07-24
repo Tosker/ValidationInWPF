1804e0cfbd5fe693d954243d931be4ba7c6c2611
Joseph <josephebenge@gmail.com>
2018-07-24 12:00:13 -0400

    Implemented custom rule and error template

---
+++ DataValidation/C_Validation_ByCustom/MinimumCharacterRule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace C_Validation_ByCustom
+{
+    public class MinimumCharacterRule : ValidationRule
+    {
+        public int MinimumCharacters { get; set; }
+
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string charString = value as string;
+
+            if (charString.Length < MinimumCharacters)
+                return new ValidationResult(false, $"User atleast {MinimumCharacters} characters.");
+
+            return new ValidationResult(true, null);
+        }
+    }
+}
