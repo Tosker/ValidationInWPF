00afc2071555241002e2df7ac25374a302693ba6
Joseph <josephebenge@gmail.com>
2018-07-24 11:47:18 -0400

    Implemented DataErrorInfo

--- DataValidation/B_Validation_ByDataErrorInfo/RegistrationVM.cs
+++ DataValidation/B_Validation_ByDataErrorInfo/RegistrationVM.cs
@@ -1,14 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace B_Validation_ByDataErrorInfo
 {
-    public class RegistrationVM : ObservableObject
+    public class RegistrationVM : ObservableObject, IDataErrorInfo
     {
+        public string Error { get { return null;  } }
         private string _username;
+
+        public Dictionary<string, string> ErrorCollection { get; private set; } = new Dictionary<string, string>();
+
+        public string this[string name]
+        {
+            get
+            {
+                string result = null;
+
+                switch(name)
+                {
+                    case "Username":
+                        if (string.IsNullOrWhiteSpace(Username))
+                            result = "Username cannot be empty";
+                        else if (Username.Length < 5)
+                            result = "Username must be a minimum of 5 characters.";
+                        break;
+                }
+
+                if (ErrorCollection.ContainsKey(name))
+                    ErrorCollection[name] = result;
+                else if (result != null)
+                    ErrorCollection.Add(name, result);
+
+                OnPropertyChanged("ErrorCollection");
+                return result;
+            }
+        }
+
         public string Username
         {
             get { return _username; }
@@ -17,5 +48,6 @@ namespace B_Validation_ByDataErrorInfo
                 OnPropertyChanged(ref _username, value);
             }
         }
+
     }
 }
