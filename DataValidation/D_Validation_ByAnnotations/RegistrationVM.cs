66995c6f5a8f920bd26cfe4c2d19523856362529
Joseph <josephebenge@gmail.com>
2018-07-24 12:10:36 -0400

    Implemented annotations

--- DataValidation/D_Validation_ByAnnotations/RegistrationVM.cs
+++ DataValidation/D_Validation_ByAnnotations/RegistrationVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,13 +10,25 @@ namespace D_Validation_ByAnnotations
     public class RegistrationVM : ObservableObject
     {
         private string _username;
+
+        [Required(ErrorMessage = "Must not be empty.")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Must be at least 5 characters.")]
         public string Username
         {
             get { return _username; }
             set
             {
+                ValidateProperty(value, "Username");
                 OnPropertyChanged(ref _username, value);
             }
+        }
+
+        private void ValidateProperty<T>(T value, string name)
+        {
+            Validator.ValidateProperty(value, new ValidationContext(this, null, null)
+            {
+                MemberName = name
+            });
         }
     }
 }
